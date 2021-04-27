30285c5953071cba9bedd0e8f0ba9967f9e9e79c
aesoper <weilanzhuan@163.com>
2021-04-27 15:23:58 +0800

    Updates

---
+++ js/about.5ed6d5b0.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,e){"use strict";e.r(t);var a=e("7a23"),c={class:"about"},o=Object(a["e"])("h1",null,"This is an about page",-1);function s(n,t){return Object(a["o"])(),Object(a["c"])("div",c,[o])}const u={};u.render=s;t["default"]=u}}]);
